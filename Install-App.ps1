92e855abafb68ffcba2081ba6e0ed24e3c3cf3e0
Econis <ryker02@gmail.com>
2020-09-23 17:57:30 -0700

    made them dot source instead of file calls

--- Install-App.ps1
+++ Install-App.ps1
@@ -18,91 +18,93 @@ function New-TempDirectory([string]$Path){
     return $Path
 }
 
-param(
-    [CmdletBinding(DefaultParameterSetName="Local")]
-    [Parameter(Mandatory=$true,Postition=0,ParameterSetName="Local")]
-    [string]$File_Path,
+function Install-App{
+    param(
+        [CmdletBinding(DefaultParameterSetName="Local")]
+        [Parameter(Mandatory=$true,Postition=0,ParameterSetName="Local")]
+        [string]$File_Path,
 
-    [Parameter(Mandatory=$true,Postition=0,ParameterSetName="Web")]
-    [string]$Download_Url,
+        [Parameter(Mandatory=$true,Postition=0,ParameterSetName="Web")]
+        [string]$Download_Url,
 
-    [Parameter(Mandatory=$true,Postition=1)]
-    [ValidateSet("exe","msi","zip/exe","zip/msi")]
-    [string]$Installer_Type = "exe",
+        [Parameter(Mandatory=$true,Postition=1)]
+        [ValidateSet("exe","msi","zip/exe","zip/msi")]
+        [string]$Installer_Type = "exe",
 
-    [string]$Arguments
+        [string]$Arguments
 
-)
+    )
 
-[string]$TEMP = New-TempDirectory($env:APPDATA)
-[string]$APP_ID = $(New-Guid).Guid
-[string]$APP_TEMP = "$TEMP\$APP_ID."
+    [string]$TEMP = New-TempDirectory($env:APPDATA)
+    [string]$APP_ID = $(New-Guid).Guid
+    [string]$APP_TEMP = "$TEMP\$APP_ID."
 
-switch($Installer_Type){ # Set the Temp File path based on installer type
-    ($Installer_Type.Contains("zip/")){
-        $APP_TEMP += $Installer_Type.remove("zip/")
-    }
-    default {
-        $APP_TEMP += $Installer_Type
-    }
-}
-
-switch($PSCmdlet.ParameterSetName){
-    "Web" {
-        try { # Download EXE and set File Path
-            Invoke-WebRequest $Download_Path -OutFile $APP_TEMP -ErrorAction Stop
+    switch($Installer_Type){ # Set the Temp File path based on installer type
+        ($Installer_Type.Contains("zip/")){
+            $APP_TEMP += $Installer_Type.remove("zip/")
         }
-        catch {
-            Write-Error "Unable to Download requested File from:[$Download_Path] to:[$APP_TEMP]"
-            Write-Error "$($_.Exception.Message)"
+        default {
+            $APP_TEMP += $Installer_Type
         }
     }
 
-    default{
-        write-host "Default: "
+    switch($PSCmdlet.ParameterSetName){
+        "Web" {
+            try { # Download EXE and set File Path
+                Invoke-WebRequest $Download_Path -OutFile $APP_TEMP -ErrorAction Stop
+            }
+            catch {
+                Write-Error "Unable to Download requested File from:[$Download_Path] to:[$APP_TEMP]"
+                Write-Error "$($_.Exception.Message)"
+            }
+        }
+
+        default{
+            write-host "Default: "
+        }
     }
-}
 
-# Set the UAC to Allow Install of this File
-$regPath = "HKCU:\Software\Classes\ms-settings\shell\open\command"
+    # Set the UAC to Allow Install of this File
+    $regPath = "HKCU:\Software\Classes\ms-settings\shell\open\command"
 
-New-Item $regPath -Force
-New-ItemProperty $regPath -Name "DelegateExecute" -Value $null -Force
-New-ItemProperty $regPath -Name "(default)" -Value $APP_TEMP -Force
+    New-Item $regPath -Force
+    New-ItemProperty $regPath -Name "DelegateExecute" -Value $null -Force
+    New-ItemProperty $regPath -Name "(default)" -Value $APP_TEMP -Force
 
-# Install File
-try{
-    $proc = Start-Process -FilePath $APP_TEMP -ArgumentList $Arguments -PassThru
-}
-catch {
+    # Install File
+    try{
+        $proc = Start-Process -FilePath $APP_TEMP -ArgumentList $Arguments -PassThru
+    }
+    catch {
+        Remove-Item $regPath -Force -Recurse
+    }
+
+    $timer = [System.Diagnostics.Stopwatch]::new()
+    $timer.Start()
+    $x = 0
+    $Status = "Installing"
+    while(!$proc.HasExited){
+
+        Write-Progress -PercentComplete $x -Status $Status -Activity "[Hours]$($timer.Elapsed.Hours) [Minutes]$($timer.Elapsed.Minutes) [Seconds]$($timer.Elapsed.Seconds)"
+
+        if($x -lt 100){
+            $x = $x + 5
+            $Status += "."
+        }
+        elseif($x -eq 20){
+            $Status = "Installing"
+        }
+        else{
+            $x = 0
+        }
+
+        Start-Sleep -Seconds 1
+
+    }
+
+    $timer.Stop()
+
+    Write-Host "Installation completed in [Hours]$($timer.Elapsed.Hours) [Minutes]$($timer.Elapsed.Minutes) [Seconds]$($timer.Elapsed.Seconds)" -ForegroundColor Green
+
     Remove-Item $regPath -Force -Recurse
 }
-
-$timer = [System.Diagnostics.Stopwatch]::new()
-$timer.Start()
-$x = 0
-$Status = "Installing"
-while(!$proc.HasExited){
-
-    Write-Progress -PercentComplete $x -Status $Status -Activity "[Hours]$($timer.Elapsed.Hours) [Minutes]$($timer.Elapsed.Minutes) [Seconds]$($timer.Elapsed.Seconds)"
-
-    if($x -lt 100){
-        $x = $x + 5
-        $Status += "."
-    }
-    elseif($x -eq 20){
-        $Status = "Installing"
-    }
-    else{
-        $x = 0
-    }
-
-    Start-Sleep -Seconds 1
-
-}
-
-$timer.Stop()
-
-Write-Host "Installation completed in [Hours]$($timer.Elapsed.Hours) [Minutes]$($timer.Elapsed.Minutes) [Seconds]$($timer.Elapsed.Seconds)" -ForegroundColor Green
-
-Remove-Item $regPath -Force -Recurse
--- Install-DotNet.ps1
+++ Install-DotNet.ps1
@@ -1,9 +1,3 @@
-param(
-    [Parameter(Mandatory=$true,Position=0)]
-    [ValidateSet("4.7.2")]
-    [string]$Version
-)
-
 function New-TempDirectory($Path){
 
     $Path += "\temp"
@@ -23,86 +17,95 @@ function New-TempDirectory($Path){
     return $Path
 }
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+function Install-DotNet{
+    param(
+        [Parameter(Mandatory=$true,Position=0)]
+        [ValidateSet("4.7.2")]
+        [string]$Version
+    )
 
-$TEMP = $(New-TempDirectory($env:APPDATA)).FullName
-$APP_NAME = "dotNET-$($Version.replace('.','_'))"
-$APP_TEMP = "$TEMP\$APP_NAME.exe"
 
-$Versions = @{
-    "4.7.2" = @{
-        init_url = "https://dotnet.microsoft.com/download/dotnet-framework/thank-you/net472-offline-installer"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    $TEMP = $(New-TempDirectory($env:APPDATA)).FullName
+    $APP_NAME = "dotNET-$($Version.replace('.','_'))"
+    $APP_TEMP = "$TEMP\$APP_NAME.exe"
+
+    $Versions = @{
+        "4.7.2" = @{
+            init_url = "https://dotnet.microsoft.com/download/dotnet-framework/thank-you/net472-offline-installer"
+        }
     }
-}
 
-function Get-DotNetDownloadLink($init_url){
-    try{
-         return ($( Invoke-WebRequest -Uri $init_url -ErrorAction Stop ).Links | ?{ $_.outerHTML -like "*click here to download manually*"}).href
+    function Get-DotNetDownloadLink($init_url){
+        try{
+            return ($( Invoke-WebRequest -Uri $init_url -ErrorAction Stop ).Links | ?{ $_.outerHTML -like "*click here to download manually*"}).href
+        }
+        catch {
+            write-host "Unable to Get Download Link" -ForegroundColor Red
+            write-host $_.Exception.Message -ForegroundColor Red
+            Exit 500
+        }
+    }
+
+    try{ # Download .Net
+        # Set the UAC to Allow Install of this File
+        $regPath = "HKCU:\Software\Classes\ms-settings\shell\open\command"
+        New-Item $regPath -Force | out-null
+        New-ItemProperty $regPath -Name "DelegateExecute" -Value $null -Force | out-null
+        New-ItemProperty $regPath -Name "(default)" -Value $APP_TEMP -Force | out-null
+
+        $link = Get-DotNetDownloadLink($versions[$Version].init_url)
+
+        Write-Host "Downloading .NET Version: $Version" -ForegroundColor Green
+
+        Invoke-WebRequest -Uri $Link -OutFile $APP_TEMP -ErrorAction Stop
     }
     catch {
-        write-host "Unable to Get Download Link" -ForegroundColor Red
+        write-host "Unable to Download Dot Net Installer" -ForegroundColor Red
         write-host $_.Exception.Message -ForegroundColor Red
         Exit 500
     }
-}
 
-try{ # Download .Net
-    # Set the UAC to Allow Install of this File
-    $regPath = "HKCU:\Software\Classes\ms-settings\shell\open\command"
-    New-Item $regPath -Force | out-null
-    New-ItemProperty $regPath -Name "DelegateExecute" -Value $null -Force | out-null
-    New-ItemProperty $regPath -Name "(default)" -Value $APP_TEMP -Force | out-null
-
-    $link = Get-DotNetDownloadLink($versions[$Version].init_url)
-
-    Write-Host "Downloading .NET Version: $Version" -ForegroundColor Green
-
-    Invoke-WebRequest -Uri $Link -OutFile $APP_TEMP -ErrorAction Stop
-}
-catch {
-    write-host "Unable to Download Dot Net Installer" -ForegroundColor Red
-    write-host $_.Exception.Message -ForegroundColor Red
-    Exit 500
-}
-
-try{ # Install .Net
-    Write-Host "Installing .NET Version: $Version" -ForegroundColor Green
-    Write-Host "Installation can take up to 10 minutes" -ForegroundColor Green
-    $proc = Start-Process -FilePath $APP_TEMP -ArgumentList "/q /norestart" -PassThru
-}
-catch {
-    write-host "Unable to Install .NET Version: $Version" -ForegroundColor Red
-    write-host $_.Exception.Message -ForegroundColor Red
-    Exit 500
-}
-
-$timer = [System.Diagnostics.Stopwatch]::new()
-$timer.Start()
-$x = 0
-$Status = "Installing..."
-while(!$proc.HasExited){
-
-    Write-Progress -PercentComplete $x -Status $Status -Activity "[Hours]$($timer.Elapsed.Hours) [Minutes]$($timer.Elapsed.Minutes) [Seconds]$($timer.Elapsed.Seconds)"
-
-    if($x -lt 100){
-        $x = $x++
-        #$Status += "."
+    try{ # Install .Net
+        Write-Host "Installing .NET Version: $Version" -ForegroundColor Green
+        Write-Host "Installation can take up to 10 minutes" -ForegroundColor Green
+        $proc = Start-Process -FilePath $APP_TEMP -ArgumentList "/q /norestart" -PassThru
     }
-    elseif($x -eq 20){
-        #$Status = "Installing"
-    }
-    else{
-        $x = 0
+    catch {
+        write-host "Unable to Install .NET Version: $Version" -ForegroundColor Red
+        write-host $_.Exception.Message -ForegroundColor Red
+        Exit 500
     }
 
-    Start-Sleep -Seconds 1
+    $timer = [System.Diagnostics.Stopwatch]::new()
+    $timer.Start()
+    $x = 0
+    $Status = "Installing..."
+    while(!$proc.HasExited){
 
+        Write-Progress -PercentComplete $x -Status $Status -Activity "[Hours]$($timer.Elapsed.Hours) [Minutes]$($timer.Elapsed.Minutes) [Seconds]$($timer.Elapsed.Seconds)"
+
+        if($x -lt 100){
+            $x = $x++
+            #$Status += "."
+        }
+        elseif($x -eq 20){
+            #$Status = "Installing"
+        }
+        else{
+            $x = 0
+        }
+
+        Start-Sleep -Seconds 1
+
+    }
+
+    $timer.Stop()
+
+    Write-Host "Installation completed in [Hours]$($timer.Elapsed.Hours) [Minutes]$($timer.Elapsed.Minutes) [Seconds]$($timer.Elapsed.Seconds)" -ForegroundColor Green
+
+    Remove-Item $regPath -Force -Recurse
+    # Need to add a Run Once to Continue Script or move to next one
+    # Prollay a running config file, that we can clean up later
 }
-
-$timer.Stop()
-
-Write-Host "Installation completed in [Hours]$($timer.Elapsed.Hours) [Minutes]$($timer.Elapsed.Minutes) [Seconds]$($timer.Elapsed.Seconds)" -ForegroundColor Green
-
-Remove-Item $regPath -Force -Recurse
-# Need to add a Run Once to Continue Script or move to next one
-# Prollay a running config file, that we can clean up later
