78a3c5351c8ff18e6a3795f7350fd53cf41d1860
Econis <ryker02@gmail.com>
2020-09-25 18:07:52 -0700

    Changed to modules, dot sourcing is a PITA

--- DotNet-Utilities.psm1
+++ DotNet-Utilities.psm1
@@ -1,5 +1,6 @@
 # Source Utilties
-. ".\Utilties.ps1"
+# . ".\Utilties.ps1"
+Import-Module -Name ".\Utilities.psm1"
 function Install-DotNet{
     param(
         [Parameter(Mandatory=$true,Position=0)]
