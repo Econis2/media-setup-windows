ee4c7aaf3e7a949dbb32f0731e7b8db5aa178992
Derek Spiner <31101671+dspiner@users.noreply.github.com>
2020-10-02 11:33:28 -0700

    start-process seemed to work with single, so applying multi now

--- classes/DownloadManager/DownloadManager.psm1
+++ classes/DownloadManager/DownloadManager.psm1
@@ -13,14 +13,20 @@ class DownloadManager {
     DownloadManager([DownloadConfig]$_config){ $this.Config = $_config }
 
     hidden [void]_Download([DownloadConfig]$Config){ 
-        # [System.Net.WebClient]::new().DownloadFileAsync($Config.Url, $Config.Path) 
-        $encoded = [Convert]::ToBase64String([System.Text.Encoding]::Unicode("[System.Net.WebClient]::new().DownloadFile(`"$Config.Url`", `"$Config.Path`")"))
+        # [System.Net.WebClient]::new().DownloadFileAsync($Config.Url, $Config.Path)
+        $cmd =
+@"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[System.Net.WebClient]::new().DownloadFile("$($Config.Url)", "$($Config.Path)").Dispose()
+"@ 
+        $encoded = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes())
         $args = @(
             "-encodedCommand $encoded",
             "-ExecutionPolicy Bypass"
+            "-WindowStyle Hidden"
         )
 
-        $proc = Start-Process "C:\windows\system32\WindowsPowershell\v1.0\powershell.exe" -ArgumentList $args -PassThru
+        $proc = Start-Process "C:\windows\system32\WindowsPowershell\v1.0\powershell.exe" -ArgumentList $args -PassThru -WindowStyle Hidden
     }
 
     [bool]DownloadFile([DownloadConfig]$Config){
@@ -37,7 +43,7 @@ class DownloadManager {
                     break
                 }
                 $percent = ( $c_length / $Length) * 100
-                Write-Progress -Id "FileName" -Activity "Downloading" -Status "$c_length of $($Length)" -PercentComplete $percent
+                Write-Progress -Id 1 -Activity "Downloading" -Status "$c_length of $($Length)" -PercentComplete $percent
 
             
             Start-Sleep -Milliseconds 50
@@ -55,11 +61,35 @@ class DownloadManager {
         return $true
     }
 
-    [bool]DownloadFiles([DownloadConfig[]]$Configs){
+    [void]DownloadFiles([DownloadConfig[]]$Configs){
+        [System.Collections.ArrayList]$ActiveDownloads = @()
         $Configs.forEach({
-            $this.DownloadFile($_)
+            $ActiveDownloads.Add(@{
+                Path = $_.Path
+                Url = $_.Url
+                Size = ([System.Net.WebRequest]::Create($Config.Url).GetResponse().Headers['Content-Length'])
+            }) | Out-Null
         })
-        return $true
+        $completed = $false
+        while(!$completed){
+            for($x = 0; $x -lt $ActiveDownloads.Count; $x++){
+                $c_length = (Get-Item $ActiveDownloads[$x].Path).Length
+                if($ActiveDownloads.Size -eq $c_length){
+                    $completed = $true
+                    break
+                }
+                $percent = ( $c_length / $ActiveDownloads.Size) * 100
+                Write-Progress -Id 1 -Activity "Downloading" -Status "$c_length of $($ActiveDownloads.Size)" -PercentComplete $percent
+
+            
+            Start-Sleep -Milliseconds 50
+            }
+        }
+
+        # $Configs.forEach({
+        #     $this.DownloadFile($_)
+        # })
+        # return $true
     }
 
     [int]getJobPercent($Job){ return [Math]::Round( ($Job.BytesTransferred / $Job.BytesTotal) * 100 ) }
