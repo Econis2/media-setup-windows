56d5ef5d522d3422c9a5129e5182325be8261fa1
Derek Spiner <31101671+dspiner@users.noreply.github.com>
2020-10-02 10:56:39 -0700

    Running each asyn doesnt work, not sure why, single to single downloads with status

--- classes/DownloadManager/DownloadManager.psm1
+++ classes/DownloadManager/DownloadManager.psm1
@@ -5,27 +5,35 @@ using module "..\Logger\LogType\LogType.psm1"
 class DownloadManager {
     [DownloadConfig[]]$Configs
     [DownloadConfig]$Config
-    [System.Collections.ArrayList]$CurrentJobs = @()# In Progress BitsTransferJobs
-    [System.Collections.ArrayList]$FinishedJobs = @()# Completed BitsTransferJobs
-    [System.Collections.ArrayList]$ErrorJobs = @()# Jobs That Errored
-    hidden [Logger]$Logger = [Logger]::new($true, $true)
 
+    hidden [Logger]$Logger = [Logger]::new($true, $true)
+    
+    DownloadManager(){}
     DownloadManager([DownloadConfig[]]$_configs){ $this.Configs = $_configs }
     DownloadManager([DownloadConfig]$_config){ $this.Config = $_config }
 
-    hidden [void]_Download([DownloadConfig]$Config){ [System.Net.WebClient]::new().DownloadDataAsync($Config.Path,$Config.Url) }
+    hidden [void]_Download([DownloadConfig]$Config){ [System.Net.WebClient]::new().DownloadDataAsync($Config.Url, $Config.Path) }
 
     [bool]DownloadFile([DownloadConfig]$Config){
         # Get File Statistics
         $Length = ([System.Net.WebRequest]::Create($_.Url).GetResponse().Headers['Content-Length'])
 
         $this._Download($Config)
+        $completed = $false
+        while(!$completed){
+            for($x = 0; $x -lt $this.CurrentJobs.Count; $x++){
+                $c_length = (Get-Item $this.CurrentJobs[$x].Path).Length
 
-        $this.CurrentJobs.Add(@{
-            Path = $Config.Path
-            Url = $Config.Url
-            Size = $Length
-        }) | Out-Null
+                if($Length -eq $c_length){
+                    $completed = $true
+                    break
+                }
+                $percent = ( $c_length / $this.CurrentJobs[$x].size) * 100
+                Write-Progress -Id $x -Activity "Downloading" -Status "$c_length of $($this.CurrentJobs[$x].size)" -PercentComplete $percent
+
+            }
+            Start-Sleep -Milliseconds 50
+        }
 
         return $true
     }
@@ -33,43 +41,18 @@ class DownloadManager {
     [bool]DownloadFile(){ return $this.DownloadFile($this.Config) }
 
     [bool]DownloadFiles(){
-        [System.Collections.Arraylist]$Jobs = @()
-        # Get File Stats
         $this.Configs.forEach({
-            $Jobs.Add(@{
-                Url = $_.Url
-                Path = $_.Path
-                Size = ([System.Net.WebRequest]::Create($_.Url).GetResponse().Headers['Content-Length'])
-            }) | Out-Null
+            $this.DownloadFile($_)
         })
-        # Start File Downloads
-        $this.Configs.forEach({
-            $this._Download($_)
-        })
-
         return $true
-
     }
 
-    [void]WatchDownloads(){
-
-        while($this.CurrentJobs.Count -ne 0){
-            for($x = 0; $x -lt $this.CurrentJobs.Count; $x++){
-                $c_length = (Get-Item $this.CurrentJobs[$x].Path).Length
-
-                if($this.CurrentJobs[$x].size -eq $c_length){
-                    $this.FinishedJobs.Add($this.CurrentJobs[$x]) | Out-Null
-                    $this.CurrentJobs.RemoveAt($x) | Out-Null
-                }
-                $percent = ( $c_length / $this.CurrentJobs[$x].size) * 100
-
-                Write-Progress -Id $x -Activity "Downloading" -Status "$c_length of $($this.CurrentJobs[$x].size)" -PercentComplete $percent
-
-            }
-
-            Start-Sleep -Milliseconds 150
-        }
-
+    [bool]DownloadFiles([DownloadConfig[]]$Configs){
+        $Configs.forEach({
+            $this.DownloadFile($_)
+        })
+        return $true
     }
+
     [int]getJobPercent($Job){ return [Math]::Round( ($Job.BytesTransferred / $Job.BytesTotal) * 100 ) }
 }
