2a2bbd4ce41c3f314724b08fd5ef86001b29100a
Econis <ryker02@gmail.com>
2020-10-01 18:07:43 -0700

    found a funtion going to try it

--- classes/DownloadManager/DownloadManager.psm1
+++ classes/DownloadManager/DownloadManager.psm1
@@ -5,7 +5,8 @@ using module "..\Logger\LogType\LogType.psm1"
 class DownloadManager {
     [DownloadConfig[]]$Configs
     [DownloadConfig]$Config
-    [System.Collections.ArrayList]$CurrentJobs = @()# In Progress BitsTransferJobs
+    #[System.Collections.ArrayList]$CurrentJobs = @()# In Progress BitsTransferJobs
+    $CurrentJob = [hashtable]::Synchronized(@{})
     [System.Collections.ArrayList]$FinishedJobs = @()# Completed BitsTransferJobs
     [System.Collections.ArrayList]$ErrorJobs = @()# Jobs That Errored
     hidden [Logger]$Logger = [Logger]::new($true, $true)
@@ -14,38 +15,94 @@ class DownloadManager {
     DownloadManager([DownloadConfig]$_config){ $this.Config = $_config }
 
     [bool]DownloadFile([DownloadConfig]$Config){
-        $this.Logger.WriteLog([LogType]::INFO, "Starting Download FROM: $($Config.Url) TO: $($Config.Path)")
-        try{
 
-            $ID = (New-Guid).Guid
-            $WC = [System.Net.WebClient]::new()
+        $wc = New-Object Net.WebClient
+        $destination = Join-Path $this.Config.Path
+        $start = Get-Date 
+        $null = Register-ObjectEvent –InputObject $wc –EventName DownloadProgressChanged `
+            –MessageData @{start=$start;includeStats=$true} `
+            –SourceIdentifier WebClient.DownloadProgressChanged –Action { 
+                filter Get-FileSize {
+                    "{0:N2} {1}" -f $(
+                    if ($_ -lt 1kb) { $_, 'Bytes' }
+                    elseif ($_ -lt 1mb) { ($_/1kb), 'KB' }
+                    elseif ($_ -lt 1gb) { ($_/1mb), 'MB' }
+                    elseif ($_ -lt 1tb) { ($_/1gb), 'GB' }
+                    elseif ($_ -lt 1pb) { ($_/1tb), 'TB' }
+                    else { ($_/1pb), 'PB' }
+                    )
+                }
+                $elapsed = ((Get-Date) – $event.MessageData.start)
+                #calculate average speed in Mbps
+                $averageSpeed = ($EventArgs.BytesReceived * 8 / 1MB) / $elapsed.TotalSeconds
+                $elapsed = $elapsed.ToString('hh\:mm\:ss')
+                #calculate remaining time considering average speed
+                $remainingSeconds = ($EventArgs.TotalBytesToReceive – $EventArgs.BytesReceived) * 8 / 1MB / $averageSpeed
+                $receivedSize = $EventArgs.BytesReceived | Get-FileSize
+                $totalSize = $EventArgs.TotalBytesToReceive | Get-FileSize        
+                Write-Progress –Activity (" $url {0:N2} Mbps" -f $averageSpeed) `
+                    –Status ("{0} of {1} ({2}% in {3})" -f $receivedSize,$totalSize,$EventArgs.ProgressPercentage,$elapsed) `
+                    –SecondsRemaining $remainingSeconds `
+                    –PercentComplete $EventArgs.ProgressPercentage
+                if ($EventArgs.ProgressPercentage -eq 100){
+                     Write-Progress –Activity (" $url {0:N2} Mbps" -f $averageSpeed) `
+                    –Status 'Done' –Completed
+                    if ($event.MessageData.includeStats.IsPresent){
+                        ([PSCustomObject]@{Name='Get-FileWCAsynchronous';TotalSize=$totalSize;Time=$elapsed}) | Out-Host
+                    }
+                }
+            }    
+        $null = Register-ObjectEvent –InputObject $wc –EventName DownloadFileCompleted `
+             –SourceIdentifier WebClient.DownloadFileCompleted –Action { 
+                Unregister-Event –SourceIdentifier WebClient.DownloadProgressChanged
+                Unregister-Event –SourceIdentifier WebClient.DownloadFileCompleted
+                Get-Item $destination | Unblock-File
+            }  
+        try  {  
+            $wc.DownloadFileAsync($this.Config.Url, $destination)  
+        }  
+        catch [System.Net.WebException]  {  
+            Write-Warning "Download of $($this.Config.Url) failed"
+            return $false
+        }   
+        finally  {    
+            $wc.Dispose()
+             
+        }  
+        return $true
+     }
+        # $this.Logger.WriteLog([LogType]::INFO, "Starting Download FROM: $($Config.Url) TO: $($Config.Path)")
+        # try{
 
-            Register-ObjectEvent -InputObject $WC -SourceIdentifier "Web.DownloadProgressChanged" -EventName "DownloadProgressChanged" -Action {
-                $Percent = [Math]::Round( 100 * ($Eventargs.BytesReceived / $Eventargs.TotalBytesToReceive))
-                New-Event -SourceIdentifier "FileDownloadUpdate-$ID" -MessageData @($Percent, $eventargs.BytesReceived, $eventargs.TotalBytesToReceive) | Out-Null
-            }
+            # $ID = (New-Guid).Guid
+            # $WC = [System.Net.WebClient]::new()
 
-            Register-ObjectEvent -InputObject $WC -SourceIdentifier "Web.DownloadFileCompleted" -EventName "DownloadFileCompleted" -Action {
-                New-Event -SourceIdentifier "FileDownloadCompleted-$ID" | out-null
-            }
+            # Register-ObjectEvent -InputObject $WC -MessageData @{id = $ID} -SourceIdentifier "Web.DownloadProgressChanged" -EventName "DownloadProgressChanged" -Action {
+            #     $Percent = [Math]::Round( 100 * ($Eventargs.BytesReceived / $Eventargs.TotalBytesToReceive))
+            #     New-Event -SourceIdentifier "FileDownloadUpdate-$ID" -MessageData @($Percent, $eventargs.BytesReceived, $eventargs.TotalBytesToReceive) | Out-Null
+            # }
 
-            $this.CurrentJobs.add($Job) | Out-Null
+            # Register-ObjectEvent -InputObject $WC -SourceIdentifier "Web.DownloadFileCompleted" -EventName "DownloadFileCompleted" -Action {
+            #     New-Event -SourceIdentifier "FileDownloadCompleted-$ID" | out-null
+            # }
 
-            $WC.DownloadFileAsync($Config.Url, $Config.Path)
+            # $this.CurrentJobs.add($Job) | Out-Null
+
+            # $WC.DownloadFileAsync($Config.Url, $Config.Path)
             
             # $this.CurrentJobs.add($ID) | Out-Null
                 
                 #$(Start-Process -File )
                 #$(Start-BitsTransfer -Source $Config.Url -Destination $Config.Path -Asynchronous -ErrorAction Stop) 
             #)
-        }
-        catch{
-            $this.Logger.WriteLog([LogType]::ERROR, "Unable to Download FROM: $($Config.Url) TO: $($Config.Path)")
-            $this.Logger.WriteLog([LogType]::ERROR, $_.Exception.Message)
-            return $false
-        }
-        return $true
-    }
+    #     }
+    #     catch{
+    #         $this.Logger.WriteLog([LogType]::ERROR, "Unable to Download FROM: $($Config.Url) TO: $($Config.Path)")
+    #         $this.Logger.WriteLog([LogType]::ERROR, $_.Exception.Message)
+    #         return $false
+    #     }
+    #     return $true
+    # }
 
     [bool]DownloadFile(){ return $this.DownloadFile($this.Config) }
 
