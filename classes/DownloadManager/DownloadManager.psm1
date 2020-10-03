b56e85b18b41e3e99e9073a07a82f35b39100626
Econis <ryker02@gmail.com>
2020-10-03 08:13:25 -0700

    test run of downloading all the media-stack dependencies

--- classes/DownloadManager/DownloadManager.psm1
+++ classes/DownloadManager/DownloadManager.psm1
@@ -24,16 +24,29 @@ class DownloadManager {
             "-ExecutionPolicy Bypass"
             "-WindowStyle Hidden"
         )
-
+        $this.Logger.WriteLog([LogType]::INFO,"Starting Download")
+        $this.Logger.WriteLog([LogType]::INFO,"From: $($Config.Url)")
+        $this.Logger.WriteLog([LogType]::INFO,"To: $($Config.Path)")
         $proc = Start-Process "C:\windows\system32\WindowsPowershell\v1.0\powershell.exe" -ArgumentList $args -PassThru -WindowStyle Hidden
+        $this.Logger.WriteLog([LogType]::INFO,"Process[$($proc.id)] started")
     }
 
     [bool]DownloadFile([DownloadConfig]$Config){
         # Get File Statistics
+        $this.Logger.WriteLog([LogType]::INFO,"Getting file stats")
+
         $Headers = ([System.Net.WebRequest]::Create($Config.Url).GetResponse().Headers)
-        $Length = $Headers['Content-Length']
-        $this._Download($Config)
+        
+        $Length = $Headers['Content-Length'] # File Size
+        
+        $this.Logger.WriteLog([LogType]::INFO,"File Size: $Length")
+        
+        $this._Download($Config) # Download the File
+        
+        $this.Logger.WriteLog([LogType]::INFO,"Waiting for download to finish")
+        
         $completed = $false
+        
         while(!$completed){
                 $c_length = (Get-Item $Config.Path).Length
 
@@ -53,28 +66,29 @@ class DownloadManager {
 
     [bool]DownloadFile(){ return $this.DownloadFile($this.Config) }
 
-    [bool]DownloadFiles(){
-        $this.Configs.forEach({
-            $this.DownloadFile($_)
-        })
-        return $true
-    }
-
     [void]DownloadFiles([DownloadConfig[]]$Configs){
         [System.Collections.ArrayList]$ActiveDownloads = @()
-        $TotalDownloads = $Configs.Count
-        $CompletedDownloads = 0
+        [int]$TotalDownloads = $Configs.Count
+        [int]$CompletedDownloads = 0
+        
         $Configs.forEach({
+            
+            $this.Logger.WriteLog([LogType]::INFO,"Getting Stats: $($_.Url)")
+            
             $Headers = ([System.Net.WebRequest]::Create($_.Url).GetResponse().Headers)
+            
             $ActiveDownloads.Add(@{
                 Path = $_.Path
                 Url = $_.Url
                 Size = $Headers['Content-Length']
             }) | Out-Null
         })
+
         $Configs.forEach({
             $this._Download($_)
         })
+
+        $this.Logger.WriteLog([LogType]::INFO,"Waiting for downloads to finish")
 
         while($ActiveDownloads.Count -ne 0){
             Write-Progress -Id 9999 -Activity "Downloading Files" -Status "$CompletedDownloads of $TotalDownloads" -PercentComplete (($CompletedDownloads / $TotalDownloads) *100)
@@ -84,7 +98,7 @@ class DownloadManager {
                 $c_length = (Get-Item $ActiveDownloads[$x].Path).Length
                 
                 if($ActiveDownloads[$x].Size -eq $c_length){
-                    Write-Progress -Id $x -Activity "$($ActiveDownloads[$x].Path.split('\')[-1])" -Status "$c_length of $c_length" -PercentComplete 100 -Completed
+                    Write-Progress -Id $x -Activity "$($ActiveDownloads[$x].Path.split('\')[-1])" -Status "$c_length of $c_length" -PercentComplete 100
                     $ActiveDownloads.RemoveAt($x)
                     $CompletedDownloads ++
                     break
@@ -98,4 +112,5 @@ class DownloadManager {
         }
     }
 
+    [void]DownloadFiles(){ $this.DownloadFiles($this.Configs) }
 }
---
+++ classes/MediaStack/MediaStack.psm1
@@ -0,0 +1,71 @@
+using module "..\Logger\Logger.psm1"
+using module "..\Logger\LoggerType\LoggerType.psm1"
+using module "..\DownloadManager\DownloadManager.psm1"
+
+class MediaStack {
+    # Public Properties
+    $Paths = @{
+        config = @{
+            user = $null
+            system = $null
+        }
+    }
+    [hashtable]$Config = @{
+        user = @{}
+        system = @{}
+    }
+
+    # Private Properties
+    hidden [Logger]$_Logger = [Logger]::new()
+
+    MediaStack([string]$SystemConfig, [string]$UserConfig){
+        $this.Paths.config.system = $SystemConfig
+        $this.Paths.config.user = $UserConfig
+    }
+
+    [void]_DownloadDependencies(){
+        $this._Logger.WriteLog([LogType]::INFO,"Collection Application Dependency details")
+        [System.Collections.ArrayList]$Apps = $this.Config.system.APPS.forEach({
+            $full_name = "$($_.name)-$($_.version).$($_.type)"
+            $url = "$($this.Config.system['BASE_URL'])/$($this.Config.system['RELEASE'])/$full_name"
+            $path = "$env:APP_TEMP\$full_name"
+            return [DownloadConfig]::new($url, $path)
+        })
+        $this._Logger.WriteLog([LogType]::INFO,"Download Dependency Applications")
+
+        while($Apps.Count -ne 0){
+            [DownloadManager]::new().DownloadFiles($Apps)
+            $this._Logger.WriteLog([LogType]::INFO,"Verifying Apps have successfully downloaded")
+            for($x=0; $x -lt $Apps.Count; $x++){
+                if(Test-Path $Apps[$x].Path){ 
+                    $this._Logger.WriteLog([LogType]::INFO,"Checking $($Apps[$x].Path)")
+                    $Apps.RemoveAt($x) | Out-Null
+                }
+            }
+        }
+    }
+
+    hidden [bool]_LoadSystemConfig(){
+        if(!$this._LoadConfig("system")){ return $false}
+        return $true
+    }
+
+    hidden [bool]_LoadUserConfig(){
+        if(!$this._LoadConfig("user")){ return $false}
+        return $true
+    }
+
+    hidden [bool]_LoadConfig([string]$Name){
+        try{
+            $this._Logger.WriteLog([LogType]::INFO,"Getting $Name config from: $($this.Paths.config[$Name])")
+            [hashtable]$_config = ConvertFrom-Json -InputObject $(Get-Content -Path $this.Paths.config[$Name] -Raw -ErrorAction Stop) -ErrorAction Stop
+            $this.Config = $_config
+        }
+        catch {
+            $this._Logger.WriteLog([LogType]::ERROR,"Error Getting $Name config from: $($this.Paths.config[$Name])")
+            $this._Logger.WriteLog([LogType]::ERROR,"$($_.Exception.Message)")
+            return $false
+        }
+        return $true   
+    }
+}
